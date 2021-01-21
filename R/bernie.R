096c9778f3dfd39d97851b5d0f74732e9d5fb991
aftoncoombs <aftoncoombs@gmail.com>
2021-01-21 11:54:47 -0800

    Exported Bernie
    - Added doc for Bernie

---
+++ R/bernie.R
@@ -0,0 +1,11 @@
+#' Bernie
+#'
+#' ASCII art of Bernie Sanders
+#'
+#' @format A list with two elements
+#' \describe{
+#'   \item{point}{Bernie is pointing}
+#'   \item{chair}{Bernie is in a chair}
+#' }
+#' @source Pointing bernie from https://www.pngfind.com/mpng/TRixTi_create-your-pic-bernie-sanders-head-png-transparent/, Bernie in chair from https://twitter.com/taber/status/1351938194024124419 and both turned to ASCII in https://manytools.org/hacker-tools/convert-images-to-ascii-art
+"bernie"
